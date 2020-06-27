96d256f73454d4d0cc55a2c61b384fef1a984a41
Roshan <icemission@gmail.com>
2020-06-27 21:08:37 +0800

    overlooked method

--- lib/receipts/delivery_order.rb
+++ lib/receipts/delivery_order.rb
@@ -56,7 +56,7 @@ module Receipts
         if logo_path.empty?
           move_down 32
         else
-          image logo_path, height: 32
+          image open(logo_path), height: 32
         end
 
         move_down 8
